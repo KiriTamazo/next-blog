ff6f834381287bd2050adb34baa758dc4125964f
username <kiritokirikaya80@gmail.com>
2023-02-23 01:52:39 +0630

    Start

---
+++ components/Navbar.jsx
@@ -0,0 +1,17 @@
+import { AppBar, Container, styled } from "@mui/material";
+import React from "react";
+
+const Menu = styled
+
+
+const Navbar = () => {
+  return (
+    <>
+      <Container>
+        <AppBar position="static" color="transparent" elevation="0"></AppBar>
+      </Container>
+    </>
+  );
+};
+
+export default Navbar;
